f509fcb732dc994a2b8ab8f856ac635984d8dcfe
Ben Busch <ben.busch@breedr.co>
2021-12-20 16:01:27 +0100

    Add eslint code actions

--- lua/config/null-ls.lua
+++ lua/config/null-ls.lua
@@ -8,6 +8,9 @@ local sources = {
   null_ls.builtins.formatting.prettier.with({
         prefer_local = "node_modules/.bin",  -- https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md
     }), 
+  null_ls.builtins.code_actions.eslint_d.with({
+        prefer_local = "node_modules/.bin",  -- https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md
+    }), 
 }
 
 null_ls.setup({ sources = sources })
